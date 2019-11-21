6ae23c023059e3265ffe012a3dac5fde2644d3fe
Nikhita03 <51832857+Nikhita03@users.noreply.github.com>
2019-11-21 22:54:37 +0530

    Add files via upload

---
+++ comp assignment.py
@@ -0,0 +1,252 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1 = [2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y1 = [6635,6699,7896,8476,7691,9204,9515,7239,6715,7017]
+plt.plot(x1, y1, label = "total number of accidents")
+x2=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y2=[1527,1877,1922,2136,2494,2573,2724,2609,2724,2609]
+plt.plot(x2, y2, label = "Number of Road Accidents - Fatal")
+x3=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y3=[1342,1561,2157,1904,2610,2536,2901,2741,2003,2005]
+plt.plot(x3, y3, label = "Number of Road Accidents - Killed")
+x4=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y4=[7071,9037,8072,6771,8562,10889,9639,10666,7333,2027]
+plt.plot(x4, y4, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("andhra pradesh")
+plt.legend()
+plt.show()
+
+x5=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y5=[40,53,51,	109,	57,	61,	40,	84,	34,	50,]
+plt.plot(x5, y5, label = "total number of accidents")
+x6=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y6=[10,	18,	14,	46,	18,	20	,13	,30,	15,	19,]
+plt.plot(x6, y6, label = "Number of Road Accidents - Fatal")
+x7=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y7=[10	,20,	15	,50	,18	,22	,13	,62	,21,	21]
+plt.plot(x7, y7, label = "Number of Road Accidents - Killed")
+x8=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y8=[32,	40  ,48	,77	,47,	55	,21	,179	,30,	58,	]
+plt.plot(x8, y8, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("arunachal pradesh")
+plt.legend()
+plt.show()
+
+x9=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y9=[678	,646	,619,	855	,957	,1130	,1278	,1436	,1508	,1712]
+plt.plot(x9, y9, label = "total number of accidents")
+x10=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y10=[211,	220	,223	,261,	325	,364	,432,	498	,489,	555]
+plt.plot(x10, y10, label = "Number of Road Accidents - Fatal")
+x11=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y11=[69	,196	,247	,232,	279	,348	,422,	457,	531	,537	]
+plt.plot(x11, y11, label = "Number of Road Accidents - killed")
+x12=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y12=[541,645	,488,	677,	774	,763	,906,	1098	,1342	,1306]
+plt.plot(x12, y12, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("assam")
+plt.legend()
+plt.show()
+
+x13=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y13=[802,	925	,1009	,1234	,1286	,1647	,1803,	1862	,1750	,1869]
+plt.plot(x13, y13, label = "total number of accidents")
+x14=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y14=[304,	335	,383,	480	,475,	634,	724,	743,	769	,945]
+plt.plot(x14, y14, label = "Number of Road Accidents - Fatal")
+x15=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y15=[315,	348,	399	,503,	533,	706,	733	,808	,820,	1052]
+plt.plot(x15, y15, label = "Number of Road Accidents - killed")
+x16=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y16=[63	,477,	629,	664,	839,	905,	1237,	1253,	1149	,1131]
+plt.plot(x16, y16, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("bihar")
+plt.legend()
+plt.show()
+
+x17=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y17=[2694	,3222	,3346	,3863	,3887,	4003,	3325	,4551	,4787	,5099]
+plt.plot(x17, y17, label = "total number of accidents")
+x18=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y18=[304,	335	,383	,480	,475	,634	,724	,743,	769	,945]
+plt.plot(x18, y18, label = "Number of Road Accidents - Fatal")
+x19=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y19=[367	,461	,550	,590,587	,615,	610	,891	,1126	,1243]
+plt.plot(x19, y19, label = "Number of Road Accidents - killed")
+x20=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y20=[98	,2474,	2972	,3393	,3846	,3891	,3834,	3688	,4054,	4295]
+plt.plot(x20, y20, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("Chattisgarh")
+plt.legend()
+plt.show()
+
+x21=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y21=[1301,	1488	,1816	,1896	,1899,	2237	,1884	,1911	,1907	,1857]
+plt.plot(x21, y21, label = "total number of accidents")
+x22=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y22=[103	,146	,157	,168	,182	,137	,121	,121	,130	,146]
+plt.plot(x22, y22, label = "Number of Road Accidents - Fatal")
+x23=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y23=[111,	155	,162	,183	,193	,152	,130	,136	,134	,154]
+plt.plot(x23, y23, label = "Number of Road Accidents - killed")
+x24=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y24=[1239  ,1323	,1417,	1289	,1192	,1303	,879	,765,	838	,865]
+plt.plot(x24, y24, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("Goa")
+plt.legend()
+plt.show()
+
+x25=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y25=[5912	,7116	,6854	,7351	,7043	,6770	,6431	,5825	,5562	,6196]
+plt.plot(x25, y25, label = "total number of accidents")
+x26=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y26=[819,	1017	,1042	,1184	,1330	,1350	,1541	,1559	,1535	,1965]
+plt.plot(x26, y26, label = "Number of Road Accidents - Fatal")
+x27=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y27=[889	,1094	,1128,	1288	,1456,	1461	,1698,	1678,	1689	,2113]
+plt.plot(x27, y27, label = "Number of Road Accidents - killed")
+x28=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y28=[6341,	7284,	6562	,7208,	6981,	6572,	6184	,5499	,5123	,5383]
+plt.plot(x28, y28, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("Gujrat")
+plt.legend()
+plt.show()
+
+x29=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y29=[1148	,1322	,1225,1255,	1180,	1172	,1084	,1113	,1131	,1178]
+plt.plot(x29, y29, label = "total number of accidents")
+x30=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y30=[236	,276	,286,	302,	297	,306,	276,	289	,300,	319]
+plt.plot(x30, y30, label = "Number of Road Accidents - Fatal")
+x31=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y31=[282	,322	,335,	343,	357,	360,	301,	307,	318,	353]
+plt.plot(x31, y31, label = "Number of Road Accidents - killed")
+x32=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y32=[1430	,1540	,1570	,1560	,1460	,1448,	1420,	1390	,1370	,1549]
+plt.plot(x32, y32, label = "Number of Road Accidents - Injured")
+plt.title("Haryana")
+plt.legend()
+plt.show()
+
+x33=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y33=[502,	614	,603,	736	,837,	756,	697,	752	,814	,742]
+plt.plot(x33, y33, label = "total number of accidents")
+x34=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y34=[76,    102,	83	,139,	186	,160,	144,	129,	165	,136]
+plt.plot(x34, y34, label = "Number of Road Accidents - Fatal")
+x35=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y35=[140	,110	,95	,170,	229,	181,144	,143,	169,	150]
+plt.plot(x35, y35, label = "Number of Road Accidents - killed")
+x36=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y36=[617,	816	,839,	931	,1213,869,	845,	1000	,1083,	1034]
+plt.plot(x36, y36, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("Himachal Pradesh")
+plt.legend()
+plt.show()
+
+x37=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y37=[912,	812,	919,	762,	1139,	1142,	1144,	1214,	1101,	1098]
+plt.plot(x37, y37, label = "total number of accidents")
+x38=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y38=[82,	68	,119	,92,	134,	157,	189,	197,	226,	202]
+plt.plot(x38, y38, label = "Number of Road Accidents - Fatal")
+x39=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y39=[87	,101	,119,	104,	146	,165	,191,	123,	256	,241]
+plt.plot(x39, y39, label = "Number of Road Accidents - killed")
+x40=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y40=[1355	,841	,1005	,857	,1290,	1291	,560,	621,	709	,725]
+plt.plot(x40, y40, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("Jammmu and Kashmir")
+plt.legend()
+plt.show()
+
+x41=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y41=[1441	,1286	,1024	,1092	,1489,	1318,	1341,	1294	,1190,	1496]
+plt.plot(x41, y41, label = "total number of accidents")
+x42=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y42=[525,	526	,292	,421,	555	,549	,600,	576,	538,	698]
+plt.plot(x42, y42, label = "Number of Road Accidents - Fatal")
+x43=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y43=[536	,412	,334,	445	,579	,622,	626	,647,	607	,742]
+plt.plot(x43, y43, label = "Number of Road Accidents - killed")
+x44=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y44=[915	,950	,915	,816	,1193,	981	,870,	925,	892,	1138]
+plt.plot(x44, y44, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("Jharkhand")
+plt.legend()
+plt.show()
+
+x45=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y45=[9990	,11487	,11118	,10731	,12133	,10780	,10588	,12356,	10752,13155]
+plt.plot(x45, y45, label = "total number of accidents")
+x46=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y46=[1457,	1755	,1803	,1700,	1872,	1821,	1891,	2271,	2473,	2803]
+plt.plot(x46, y46, label = "Number of Road Accidents - Fatal")
+x47=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y47=[1566	,1912	,1824	,1837,	2041	,1961	,2081	,2406,	3040,	2993]
+plt.plot(x47, y47, label = "Number of Road Accidents - killed")
+x48=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y48=[11611	,13603	,12344	,12791,	13490,	12765,	12565,	13577,	13930,	14016]
+plt.plot(x48, y48, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("Karnataka")
+plt.legend()
+plt.show()
+
+x49=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y49=[13684,	13141	,12327,	11727	,11643	,11685	,12019,	11701,	12113,	13093]
+plt.plot(x49, y49, label = "total number of accidents")
+x50=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y50=[962	,860	,903	,882	,904	,965	,996	,985,	951,	979]
+plt.plot(x50, y50, label = "Number of Road Accidents - Fatal")
+x51=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y51=[1065,	898	,929	,909,	938,	983,	1018,	1010	,963	,998]
+plt.plot(x51, y51, label = "Number of Road Accidents - killed")
+x52=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y52=[15564	,13155,	12082	,11453,	11495,	11520,	11740	,11326,	11787	,12692]
+plt.plot(x52, y52, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("Kerala")
+plt.legend()
+plt.show()
+
+x53=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y53=[10664,	12690	,12893	,15659	,17071,	15742	,16693	,18535	,19688,	19968]
+plt.plot(x53, y53, label = "total number of accidents")
+x54=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y54=[692	,957	,1027,	1240,	1620,	1434,	1689	,2034,	2024	,2180]
+plt.plot(x54, y54, label = "Number of Road Accidents - Fatal")
+x55=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y55=[2450,	2392	,2231,	1231	,2666	,3406,	3477,	3500,	4068	,4514]
+plt.plot(x55, y55, label = "Number of Road Accidents - killed")
+x56=[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+y56=[222	,9399	,9352	,9455	,7454	,8564	,10874	,10440,	10052,	11310]
+plt.plot(x56, y56, label = "Number of Road Accidents - Injured")
+plt.xlabel('years')  
+plt.ylabel('number of accidents')
+plt.title("Madhya Prdesh")
+plt.legend()
+plt.show()
